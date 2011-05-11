4e0951314f115ae08c58ae0ff37cb52bce687eab
Konstantin Boudnik <cos@apache.org>
2011-05-11 14:47:39 -0700

    Making some meaningful configuration

--- conf/hadoop-env.sh
+++ conf/hadoop-env.sh
@@ -6,52 +6,57 @@
 # remote nodes.
 
 # The java implementation to use.  Required.
-# export JAVA_HOME=/usr/lib/j2sdk1.6-sun
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
 
 # Extra Java CLASSPATH elements.  Optional.
-# export HADOOP_CLASSPATH="<extra_entries>:$HADOOP_CLASSPATH"
+#export HADOOP_CLASSPATH=
 
 # The maximum amount of heap to use, in MB. Default is 1000.
-# export HADOOP_HEAPSIZE=2000
+export HADOOP_HEAPSIZE=2000
 
 # Extra Java runtime options.  Empty by default.
-# if [ "$HADOOP_OPTS" == "" ]; then export HADOOP_OPTS=-server; else HADOOP_OPTS+=" -server"; fi
+#export HADOOP_OPTS=
 
 # Command specific options appended to HADOOP_OPTS when specified
+export HADOOP_NAMENODE_OPTS=-Xmx4096m
+export HADOOP_SECONDARYNAMENODE_OPTS=-Xmx4096m
+export HADOOP_JOBTRACKER_OPTS=-Xmx4096m
+#export HADOOP_DATANODE_OPTS=
+#export HADOOP_BALANCER_OPTS=
+#export HADOOP_TASKTRACKER_OPTS=
+
+### These aren't optional: com.sun.management.jmxremote can't be turned off
 export HADOOP_NAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_NAMENODE_OPTS"
 export HADOOP_SECONDARYNAMENODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_SECONDARYNAMENODE_OPTS"
 export HADOOP_DATANODE_OPTS="-Dcom.sun.management.jmxremote $HADOOP_DATANODE_OPTS"
 export HADOOP_BALANCER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_BALANCER_OPTS"
 export HADOOP_JOBTRACKER_OPTS="-Dcom.sun.management.jmxremote $HADOOP_JOBTRACKER_OPTS"
-# export HADOOP_TASKTRACKER_OPTS=
+
 # The following applies to multiple commands (fs, dfs, fsck, distcp etc)
-# export HADOOP_CLIENT_OPTS
+#export HADOOP_CLIENT_OPTS
 
 # Extra ssh options.  Empty by default.
-# export HADOOP_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=HADOOP_CONF_DIR"
+#export HADOOP_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=HADOOP_CONF_DIR"
 
 # Where log files are stored.  $HADOOP_HOME/logs by default.
-# export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
+#export HADOOP_LOG_DIR=${HADOOP_HOME}/logs
 
 # File naming remote slave hosts.  $HADOOP_HOME/conf/slaves by default.
-# export HADOOP_SLAVES=${HADOOP_HOME}/conf/slaves
+#export HADOOP_SLAVES=
 
 # host:path where hadoop code should be rsync'd from.  Unset by default.
-# export HADOOP_MASTER=master:/home/$USER/src/hadoop
+#export HADOOP_MASTER=
 
 # Seconds to sleep between slave commands.  Unset by default.  This
 # can be useful in large clusters, where, e.g., slave rsyncs can
 # otherwise arrive faster than the master can service them.
-# export HADOOP_SLAVE_SLEEP=0.1
+#export HADOOP_SLAVE_SLEEP=
 
 # The directory where pid files are stored. /tmp by default.
-# export HADOOP_PID_DIR=/var/hadoop/pids
+#export HADOOP_PID_DIR=
 
 # A string representing this instance of hadoop. $USER by default.
-# export HADOOP_IDENT_STRING=$USER
+#export HADOOP_IDENT_STRING=$USER
 
 # The scheduling priority for daemon processes.  See 'man nice'.
-# export HADOOP_NICENESS=10
-
-# Allow Hadoop to run with sysctl net.ipv6.bindv6only = 1
-# export HADOOP_ALLOW_IPV6=yes
+#export HADOOP_NICENESS=
